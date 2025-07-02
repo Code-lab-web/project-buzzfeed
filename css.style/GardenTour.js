35af4c2f6450f3046889c8d05795c06094d3f1d7
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-07-02 16:24:21 +0200

    Implement Customized Accordion component and enhance GardenTour layout with new sections

--- css.style/GardenTour.js
+++ css.style/GardenTour.js
@@ -1,6 +1,14 @@
-import { Container } from "@mui/material/Container";;
+import { Typography } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box;
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordions from "../components/Accordian";
+import CustomizedAccordions from "../components/Accordian";
+import Paper from "@mui/material/Paper"
+import BottomNavigation from "@mui/material/BottomNavigation"
+
 const Tour = () => <Container sx={{ width: 900 }}hello</hello</Container>;
-<yTypography variant="h3" component="h1" marginTop={3}></yTypography></Container>>
+<yTypography variant="h3" component="h1" marginTop={3}>
 Explore gardens
 </Typography>
 <Box marginTop={3} sx={{display: "flex"}}>
@@ -15,10 +23,28 @@ Explore gardens
     <Typography variant="h43 component="h1" marginTop={3}>
         Explore the World Gardens in Stockholm
         <Typography variant="paragraph" component="p" marginTop={3}>
-            About this garden tour Lourem 40
+            About this garden tour (Lourem 40)
 </Typography>
 </Box>
-</Container>;
+</Container>
+<Box>
+    <Box marginBottom={10}></Box>
+    <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>>
+
+</Typography>
 Frequently Asked Questions
+
+</Typography>
+<CustomizedAccordions
+</Box>
+</Container>
+<Paper
+sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+elevation={3}
+>
+    <BottomNavigation showLabels>hello</BottomNavigation>
+</Paper>
+</Container>
+);
 
 export default GardenTour;
