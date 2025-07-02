101ff4890ad0a2bb755cc7914cde9a08ef8984e0
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-07-02 12:06:22 +0200

    Refactor App component layout and fix JSON data structure for gardens

--- css.style/App.js
+++ css.style/App.js
@@ -1,36 +1,32 @@
-import SGG.png from './SGG.png';
+import SGG from './SGG.png';
 import './App.css';
 import GardenCard from "./components/GardenCard"
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
-import Grid from "@mui/material/Grid"
-import { Container, Grid }from "@mui/material"
+import Grid from "@mui/material/Grid";
 function App() {
     return (
         <div className="App">
             <SearchAppBar />
-            <Container margin sx = margin {5}>
-                <Grid container spacing={(5>
+            <Container sx={{ margin: 5 }}></Container>
+            <Container sx={{ margin: 5 }}>
+                <Grid container spacing={5}>
                     <GardenCard />
-                    <GardenCard/>
-                    <GardenCard/>
-                    <GardenCard/>
-                    </Grid>
-                )}
-            
-            <Container>
-                <Garden container spacing={5}>
                     <GardenCard />
-                    </Grid>
-                </Garden>
+                    <GardenCard />
+                    <GardenCard />
+                </Grid>
             </Container>
-            <header className="App-header">
+            <Container>
+                <Grid container spacing={5}>
+                    <GardenCard />
+                </Grid>
+            </Container>
+            <header>
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</</code> and save to reload.
-                    </p>
+                    Edit <code>src/App.js</code> and save to reload.
                     <a
-                    className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
