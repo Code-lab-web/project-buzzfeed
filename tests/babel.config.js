f53640f47cc276397f4c18491226b11eb6d0cad7
Code-lab-web <johanconsult@protonmail.com>
2025-09-18 13:47:01 +0000

    git commit -m "Code-lab-web"

--- tests/babel.config.js
+++ tests/babel.config.js
@@ -1,18 +1,6 @@
-module.exports ={
-    presents : [['@babel/present-env', {targets:{node:{node'current'}}]],
-
-},
-module.exports = api => {
-    const isTest = api.env('test');
-    // You can use isTest to determine what presets and plugins to use.
-  
-    return {
-      // ...
-    };
-  };
-  module.exports = {
+module.exports = {
     presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      '@babel/preset-typescript',
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        '@babel/preset-typescript',
     ],
-  };
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/postcss'
+
 export default defineConfig({
+  root: 'my-project',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: 'my-project/index.html'
+    }
+  }
 })
