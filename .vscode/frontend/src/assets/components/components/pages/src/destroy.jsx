b1dd48647a7543d5097bff6068851241aec4569a
Code-lab-web <johanconsult@protonmail.com>
2025-09-18 13:24:53 +0000

    git commit -m "Code-lab-web"

--- .vscode/frontend/src/assets/components/components/pages/src/destroy.jsx
+++ .vscode/frontend/src/assets/components/components/pages/src/destroy.jsx
@@ -1,16 +1,7 @@
-import { useRouteError } from "react-router-dom";
+import { redirect } from "react-router-dom";
+import { deleteContact } from "../contacts";
 
-export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
-
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+export async function action({ params }) {
+  await deleteContact(params.contactId);
+  return redirect("/");
 }
